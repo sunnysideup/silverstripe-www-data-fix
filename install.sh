621dfed8f64162206c917a0bd926fa26148c407d
Nicolaas <github@sunnysideup.co.nz>
2020-03-18 07:02:09 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,26 @@
-LINE='alias www-owns-assets="sudo chown www-data public/assets -R"'
+LINE1='alias apache-owns-assets="sudo chown www-data public/assets -R"'
+LINE2='alias cli-owns-assets="sudo chown $USER public/assets -R"'
 
 FILE="$HOME/.bash_aliases"
 
 touch $FILE
 
-grep -qsFx -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+grep -qsFx -- "$LINE1" "$FILE" || echo "$LINE1" >> "$FILE"
 
 echo "-----------------------------";
 echo "ADDED:";
-echo "... $LINE";
+echo "... $LINE1";
+echo "-----------------------------";
+echo "TO:";
+echo "... $FILE";
+echo "-----------------------------";
+
+
+grep -qsFx -- "$LINE2" "$FILE" || echo "$LINE2" >> "$FILE"
+
+echo "-----------------------------";
+echo "ADDED:";
+echo "... $LINE2";
 echo "-----------------------------";
 echo "TO:";
 echo "... $FILE";
